9441f1cb6a7e8f5d10d38b5a423da4d9defaa198
carlosgaxiola <2016030023@upsin.edu.mx>
2019-03-02 14:54:03 -0700

    modificacion de crud controller

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -3,8 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Crud extends CI_Controller {
 
-	protected $table;
-	protected $uniqueFields = array();
+	protected $tabla;
+	protected $camposUnicos = array();
+	protected $titulo;
 
 	public function __construct () {
 		parent::__construct();
@@ -12,6 +13,10 @@ class Crud extends CI_Controller {
 	}
 
 	public function index () {
-		
+		$data = array(
+			'titulo' => $this->titulo,
+			'modulos' => getModulos(),
+			'datos' => $this->CrudModel->list($this->tabla);
+		);
 	}
 }
