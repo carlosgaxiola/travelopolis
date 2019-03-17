afcb571a56a44dc572d2175daff2bff63663b482
carlosgaxiola <2016030023@upsin.edu.mx>
2019-03-17 14:51:00 -0700

    Update login_vista.php

--- application/views/Inicio/login_vista.php
+++ application/views/Inicio/login_vista.php
@@ -2,14 +2,14 @@
 	<div class="alert alert-danger" hidden id="msg-error">
 	</div>
 	<form id="frmLogin">			
-		<fieldset class="form-group row">
+		<div class="fieldset form-group row">
 			<input type="text" class="texto required" name="txtUsuario" id="txtUsuario">
 			<label for="txtUsuario">Usuario</label>
-		</fieldset>					
-		<fieldset class="form-group row">
+		</div>					
+		<div class="fieldset form-group row">
 			<input type="password" class="contra required" id="txtContra" name="txtContra">
 			<label for="txtContra">Contraseña</label>
-		</fieldset>			
+		</div>			
 		<button type="button" class="btn btn-primary" id="btn-login">Entrar</button>
 	</form>
 </div>
