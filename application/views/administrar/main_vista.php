2d9cadf0c7a35e029ae6d074ccc62f05202406cc
carlosgaxiola <44649456+carlosgaxiola@users.noreply.github.com>
2019-03-20 16:32:39 -0700

    creacion
    Se creo el archivo main vista para que carge asincronamete las diferentes tablas y formularios de cada modulo administrable

---
+++ application/views/administrar/main_vista.php
@@ -0,0 +1,5 @@
+<?php $this->load->view("Global/header", array("titulo" => "Administrar")) ?>
+<main class="content-wrapper">
+	<iframe src="https://www.upsin.edu.mx" frameborder="0"></iframe>	
+</main>
+<?php $this->load->view("Global/footer") ?>
