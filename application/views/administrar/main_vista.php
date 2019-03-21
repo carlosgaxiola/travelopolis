5397785dcdc077d652fa32021abc9ab3b38d53bc
carlosgaxiola <44649456+carlosgaxiola@users.noreply.github.com>
2019-03-20 17:17:30 -0700

    cambio
    se adapto el archivo para que sirva de contenedor de la tabla y formulario de todos los modulos

--- application/views/administrar/main_vista.php
+++ application/views/administrar/main_vista.php
@@ -1,5 +1,43 @@
-<?php $this->load->view("Global/header", array("titulo" => "Administrar")) ?>
-<main class="content-wrapper">
-	<iframe src="https://www.upsin.edu.mx" frameborder="0"></iframe>	
+<?php 
+	$this->load->view("Global/header", array("titulo" => "Administrar"));
+	$this->load->view("Global/aside", array("actual" => $modulo));	
+	if (strcmp($modulo['nombre'], "Administar") != 1)
+		$nombre = $modulo['nombre'];		
+?>
+<main class="content-wrapper">	
+	<?php if (isset($nombre)): ?>
+		<div class="content" id="tabla">
+			<div class="container-fluid">
+				<div class="row">
+					<blockquote style=" border-left: 5px solid #264d78;">
+						<?php if (isset($allowAdd)): ?>
+							<button title="<?php $allosAdd['btn-title'] ?>" id="<?php echo $allowAdd['btn-id'] ?>" class="btn btn-primary"><i class="fas fa-plus"></i></button>
+						<?php endif; ?>
+						<i class="fas fa-plus"></i></button> 							
+						<h1 class="text-justify"><?php echo $nombre ?></h1>
+						<small><?php echo $modulo['descripcion'] ?></small>
+					</blockquote>
+				</div>
+				<div class="row">
+					<div class="box box-primary">
+		                <div class="box-header with-border">
+		                    <h3 class="box-title"><i class="fa fa-list"></i> Listado de <?php echo $nombre ?></h3>
+		                    <div class="box-tools pull-right">
+		                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+		                    </div>
+		                </div>
+		                <div class="box-body">
+		                	<div class="table-responsive">
+		                		<?php $this->load->view("administar/".$nombre."/".$nombre."_tabla_vista", $registros) ?>
+		                	</div>
+		                </div>                
+					</div>
+				</div>
+			</div>
+		</div>	
+		<div class="content" id="formulario" hidden>
+			<?php $this->load->view("administar/".$nombre."/".$nombre."_formulario_vista", $extras) ?>
+		</div>
+	<?php endif; ?>
 </main>
 <?php $this->load->view("Global/footer") ?>
