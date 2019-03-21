4df086fc67618314c69fdcd97617eab907c4374f
carlosgaxiola <44649456+carlosgaxiola@users.noreply.github.com>
2019-03-20 17:55:56 -0700

    cambio de nombre

---
+++ application/views/administrar/perfiles/tabla.php
@@ -0,0 +1,24 @@
+<table id="tblPerfiles" class="table no-margin table-striped" style="width:100%">
+	<thead>                        		 	
+        <tr> 
+            <th>#</th>    					                
+            <th>Nombre</th>
+            <th>Descripción</th>
+            <th>Opciones</th>
+        </tr>
+    </thead>
+    <tbody id="contenidoTabla">
+    	<?php if (isset($registros) and !empty($registros) and is_array($registros)): ?>
+    		<?php foreach ($registros as $index => $perfil): ?>
+    			<tr>
+                    <td><?php echo $index + 1 ?></td>
+    				<td><?php echo $perfil['nombre'] ?></td>
+    				<td><?php echo $perfil['descripcion'] ?></td>
+    				<td>
+    					<button title="Editar perfil" type="button" class="btn-edit-perfil btn btn-warning" data-id-perfil="<?php echo $perfil['id'] ?>"><i class="fas fa-edit"></i></button>
+    				</td>
+    			</tr>
+    		<?php endforeach; ?>
+    	<?php endif; ?>
+    </tbody>
+</table>
