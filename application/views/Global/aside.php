1ac9641fc899cd512af0ba1baec57d5dd6ac56ad
carlosgaxiola <44649456+carlosgaxiola@users.noreply.github.com>
2019-03-20 17:56:06 -0700

    cambio

--- application/views/Global/aside.php
+++ application/views/Global/aside.php
@@ -1,8 +1,12 @@
-<aside class="main-sidebar" style="background-color: #aaaaaa; margin-top: 9%;">
+<aside class="main-sidebar bg-white">
     <section class="sidebar">
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?php echo base_url("assets/images/logo.png") ?>" class="img-circle" alt="User Image">
+                <?php if ($this->session->userdata("admin_active")): ?>
+                    <img src="<?php echo base_url("assets/images/users/".$this->session->userdata("url_foto_perfil")."/profile_60x60.jpg") ?>" class="user-image img-circle" alt="User Image">
+                <?php else: ?>
+                    <img src="<?php echo base_url("assets/images/logo.png") ?>" class="img-circle" alt="User Image">
+                <?php endif; ?>
             </div>
             <div class="pull-left info">
                 <p><?php echo $this->session->userdata("usuario") ?></p>
