bfce75df2b8ab65bcb2d6eb4e3a845c78c67197d
carlosgaxiola <44649456+carlosgaxiola@users.noreply.github.com>
2019-03-21 17:45:34 -0700

    modulo de modulos acabado

---
+++ application/views/administrar/modulos/tabla.php
@@ -0,0 +1,26 @@
+<table id="tblModulos" class="table no-margin table-striped" style="width:100%">
+	<thead>
+        <th>#</th>    					                
+        <th>Nombre</th>
+        <th>Descripción</th>
+        <th>Ruta</th>
+        <th>Icono</th>
+        <th>Opciones</th>        
+    </thead>
+    <tbody id="contenidoTabla">        
+    	<?php if (isset($registros) and !empty($registros) and is_array($registros)): ?>
+    		<?php foreach ($registros as $index => $modulo): ?>
+    			<tr data-id="<?php echo $modulo['id'] ?>">
+                    <td><?php echo $index + 1 ?></td>
+    				<td><?php echo $modulo['nombre'] ?></td>
+    				<td><?php echo $modulo['descripcion'] ?></td>
+                    <td><?php echo $modulo['ruta']?></td>
+                    <td><?php echo $modulo['fa_icon'] ?></td>
+    				<td>                        
+    					<button title="Editar modulo" type="button" class="btn-edit-log btn btn-warning" data-id="<?php echo $modulo['id'] ?>"><i class="fas fa-edit"></i></button>
+    				</td>
+    			</tr>
+    		<?php endforeach; ?>
+    	<?php endif; ?>
+    </tbody>
+</table>
