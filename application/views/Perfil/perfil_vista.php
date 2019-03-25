17630b3efa9b0981cc32945b0508cbbcf45af77e
carlosgaxiola <2016030023@upsin.edu.mx>
2019-03-25 01:13:20 -0700

    se cambio los session por usario

--- application/views/Perfil/perfil_vista.php
+++ application/views/Perfil/perfil_vista.php
@@ -4,166 +4,50 @@
 		<div class="col-md-3">
 			<div class="box box-primary">
 				<div class="box-body box-profile">
-					<?php if ($this->session->userdata("url_foto_perfil")): ?>
-						<img class="profile-user-img img-responsive img-circle" src="<?php echo base_url("assets/images/users/".$this->session->userdata("url_foto_perfil")."/profile_160x160.jpg") ?>" alt="Foto de perfil">
+					<?php if ($usuario["url_foto_perfil"]): ?>
+						<img class="profile-user-img img-responsive img-circle" src="<?php echo base_url("assets/images/users/".$usuario["url_foto_perfil"]."/profile_160x160.jpg") ?>" alt="Foto de perfil">
 					<?php else: ?>
 						<img class="profile-user-img img-responsive img-circle" src="<?php echo base_url("assets/images/users/default.png") ?>" alt="Foto de perfil">
 					<?php endif; ?>
-					<h3 class="profile-username text-center"><?php echo $this->session->userdata("completo") ?></h3>
-					<p class="text-muted text-center"><?php echo $this->session->userdata("perfil") ?></p>
+					<h3 class="profile-username text-center"><?php echo $usuario["completo"] ?></h3>
+					<p class="text-muted text-center"><?php echo $usuario["perfil"] ?></p>
 					<ul class="list-group list-group-unbordered">
 						<li class="list-group-item">
-							<b>Followers</b> <a class="pull-right">1,322</a>
-						</li>
-						<li class="list-group-item">
-							<b>Following</b> <a class="pull-right">543</a>
-						</li>
-						<li class="list-group-item">
-							<b>Friends</b> <a class="pull-right">13,287</a>
+							<b>Amigos</b> <a class="pull-right"><?php echo isset($amigos['total'])? $amigos['total']: '0' ?></a>
 						</li>	
+						<li class="list-group-item">
+							<b>Viajes realizados</b> <a class="pull-right"><?php echo isset($viajes['total'])? $viajes["total"]: '0'?></a>
+						</li>
+						<li class="list-group-item">
+							<b>Viajes deseados</b> <a class="pull-right"><?php echo isset($viaje['me_gustan'])? $viaje['me_gustan']: '0' ?></a>
+						</li>
 					</ul>
-					<a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+					<?php if (strcmp($usuario['nombre'], $this->session->userdata("nombre")) != 0): ?>
+						<a href="#" class="btn btn-primary btn-block"><b>Solicitar amistad</b></a>
+					<?php endif; ?>
 				</div>
 			</div>				
 			<div class="box box-primary">
 				<div class="box-header with-border">
-					<h3 class="box-title">About Me</h3>
+					<h3 class="box-title">Acerca de <?php echo $usuario["nombre"] ?></h3>
 				</div>		
-				<div class="box-body">
-					<strong><i class="fa fa-book margin-r-5"></i> Education</strong>
+				<div class="box-body">					
 					<p class="text-muted">
-						B.S. in Computer Science from the University of Tennessee at Knoxville
-					</p>
-					<hr>
-					<strong><i class="fa fa-map-marker margin-r-5"></i> Location</strong>
-					<p class="text-muted">Malibu, California</p>
-					<hr>
-					<strong><i class="fa fa-pencil margin-r-5"></i> Skills</strong>
-					<p>
-						<span class="label label-danger">UI Design</span>
-						<span class="label label-success">Coding</span>
-						<span class="label label-info">Javascript</span>
-						<span class="label label-warning">PHP</span>
-						<span class="label label-primary">Node.js</span>
-					</p>
-					<hr>
-					<strong><i class="fa fa-file-text-o margin-r-5"></i> Notes</strong>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+						<?php echo $usuario['descripcion'] ?>
+					</p>					
 				</div>		
 			</div>		
 		</div>	
 		<div class="col-md-9">
 			<div class="nav-tabs-custom">
-				<ul class="nav nav-tabs">
-					<li class=""><a href="#activity" data-toggle="tab" aria-expanded="false">Activity</a></li>
-					<li class="active"><a href="#timeline" data-toggle="tab" aria-expanded="true">Timeline</a></li>
-					<li class=""><a href="#settings" data-toggle="tab" aria-expanded="false">Settings</a></li>
+				<ul class="nav nav-tabs">					
+					<li class="active"><a href="#timeline" data-toggle="tab" aria-expanded="true">Viajes</a></li>
+					<?php if (strcmp($usuario['nombre'], $this->session->userdata("usuario")) == 0): ?>						
+						<li class=""><a href="#settings" data-toggle="tab" aria-expanded="false">Configuración</a></li>	
+					<?php endif; ?>
 				</ul>
-				<div class="tab-content">
-					<div class="tab-pane" id="activity">
-						<div class="post">
-							<div class="user-block">
-								<img class="img-circle img-bordered-sm" src="../../dist/img/user1-128x128.jpg" alt="user image">
-								<span class="username">
-									<a href="#">Jonathan Burke Jr.</a>
-									<a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
-								</span>
-								<span class="description">Shared publicly - 7:30 PM today</span>
-							</div>		
-							<p>
-								Lorem ipsum represents a long-held tradition for designers,
-								typographers and the like. Some people hate it and argue for
-								its demise, but others ignore the hate as they create awesome
-								tools to help create filler text for everyone from bacon lovers
-								to Charlie Sheen fans.
-							</p>
-							<ul class="list-inline">
-								<li>
-									<a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a>
-								</li>
-								<li>
-									<a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
-								</li>
-								<li class="pull-right">
-									<a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> Comments
-									(5)</a>
-								</li>
-							</ul>
-							<input class="form-control input-sm" type="text" placeholder="Type a comment">
-						</div>
-						<div class="post clearfix">
-							<div class="user-block">
-								<img class="img-circle img-bordered-sm" src="../../dist/img/user7-128x128.jpg" alt="User Image">
-								<span class="username">
-									<a href="#">Sarah Ross</a>
-									<a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
-								</span>
-								<span class="description">Sent you a message - 3 days ago</span>
-							</div>		
-							<p>
-								Lorem ipsum represents a long-held tradition for designers,
-								typographers and the like. Some people hate it and argue for
-								its demise, but others ignore the hate as they create awesome
-								tools to help create filler text for everyone from bacon lovers
-								to Charlie Sheen fans.
-							</p>
-							<form class="form-horizontal">
-								<div class="form-group margin-bottom-none">
-									<div class="col-sm-9">
-										<input class="form-control input-sm" placeholder="Response">
-									</div>
-									<div class="col-sm-3">
-										<button type="submit" class="btn btn-danger pull-right btn-block btn-sm">Send</button>
-									</div>
-								</div>
-							</form>
-						</div>		
-						<div class="post">
-							<div class="user-block">
-								<img class="img-circle img-bordered-sm" src="../../dist/img/user6-128x128.jpg" alt="User Image">
-								<span class="username">
-									<a href="#">Adam Jones</a>
-									<a href="#" class="pull-right btn-box-tool"><i class="fa fa-times"></i></a>
-								</span>
-								<span class="description">Posted 5 photos - 5 days ago</span>
-							</div>		
-							<div class="row margin-bottom">
-								<div class="col-sm-6">
-									<img class="img-responsive" src="../../dist/img/photo1.png" alt="Photo">
-								</div>				
-								<div class="col-sm-6">
-									<div class="row">
-										<div class="col-sm-6">
-											<img class="img-responsive" src="../../dist/img/photo2.png" alt="Photo">
-											<br>
-											<img class="img-responsive" src="../../dist/img/photo3.jpg" alt="Photo">
-										</div>
-										<!-- /.col -->
-										<div class="col-sm-6">
-											<img class="img-responsive" src="../../dist/img/photo4.jpg" alt="Photo">
-											<br>
-											<img class="img-responsive" src="../../dist/img/photo1.png" alt="Photo">
-										</div>		
-									</div>		
-								</div>		
-							</div>		
-							<ul class="list-inline">
-								<li>
-									<a href="#" class="link-black text-sm"><i class="fa fa-share margin-r-5"></i> Share</a>
-								</li>
-								<li>
-									<a href="#" class="link-black text-sm"><i class="fa fa-thumbs-o-up margin-r-5"></i> Like</a>
-								</li>
-								<li class="pull-right">
-									<a href="#" class="link-black text-sm"><i class="fa fa-comments-o margin-r-5"></i> 
-										Comments (5)
-									</a>
-								</li>
-							</ul>
-							<input class="form-control input-sm" type="text" placeholder="Type a comment">
-						</div>		
-					</div>		
-					<div class="tab-pane active" id="timeline">		
+				<div class="tab-content">					
+					<div class="tab-pane active" id="timeline">
 						<ul class="timeline timeline-inverse">		
 							<li class="time-label">
 								<span class="bg-red">
@@ -173,18 +57,7 @@
 							<li>
 								<i class="fa fa-envelope bg-blue"></i>
 								<div class="timeline-item">
-									<span class="time"><i class="fa fa-clock-o"></i> 12:05</span>
-									<h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
-									<div class="timeline-body">
-										Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
-										weebly ning heekya handango imeem plugg dopplr jibjab, movity
-										jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
-										quora plaxo ideeli hulu weebly balihoo...
-									</div>
-									<div class="timeline-footer">
-										<a class="btn btn-primary btn-xs">Read more</a>
-										<a class="btn btn-danger btn-xs">Delete</a>
-									</div>
+									<?php $this->load->view("Viajes/viaje_card_horizontal", array("viaje" => $viajes[0])) ?>
 								</div>
 							</li>		
 							<li>
@@ -195,7 +68,7 @@
 										<a href="#">Sarah Young</a> accepted your friend request
 									</h3>
 								</div>
-							</li>		
+							</li>
 							<li>
 								<i class="fa fa-comments bg-yellow"></i>
 								<div class="timeline-item">
@@ -210,12 +83,12 @@
 										<a class="btn btn-warning btn-flat btn-xs">View comment</a>
 									</div>
 								</div>
-							</li>		
+							</li>
 							<li class="time-label">
 								<span class="bg-green">
 									3 Jan. 2014
 								</span>
-							</li>		
+							</li>
 							<li>
 								<i class="fa fa-camera bg-purple"></i>
 								<div class="timeline-item">
@@ -229,60 +102,59 @@
 										<img src="http://placehold.it/150x100" alt="..." class="margin">
 									</div>
 								</div>
-							</li>		
+							</li>
 							<li>
 								<i class="fa fa-clock-o bg-gray"></i>
 							</li>
 						</ul>
-					</div>		
-					<div class="tab-pane" id="settings">
-						<form class="form-horizontal">
-							<div class="form-group">
-								<label for="inputName" class="col-sm-2 control-label">Name</label>
-								<div class="col-sm-10">
-									<input type="email" class="form-control" id="inputName" placeholder="Name">
-								</div>
-							</div>
-							<div class="form-group">
-								<label for="inputEmail" class="col-sm-2 control-label">Email</label>
-								<div class="col-sm-10">
-									<input type="email" class="form-control" id="inputEmail" placeholder="Email">
-								</div>
-							</div>
-							<div class="form-group">
-								<label for="inputName" class="col-sm-2 control-label">Name</label>
-								<div class="col-sm-10">
-									<input type="text" class="form-control" id="inputName" placeholder="Name">
-								</div>
-							</div>
-							<div class="form-group">
-								<label for="inputExperience" class="col-sm-2 control-label">Experience</label>
-								<div class="col-sm-10">
-									<textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
-								</div>
-							</div>
-							<div class="form-group">
-								<label for="inputSkills" class="col-sm-2 control-label">Skills</label>
-								<div class="col-sm-10">
-									<input type="text" class="form-control" id="inputSkills" placeholder="Skills">
-								</div>
-							</div>
-							<div class="form-group">
-								<div class="col-sm-offset-2 col-sm-10">
-									<div class="checkbox">
-										<label>
-											<input type="checkbox"> I agree to the <a href="#">terms and conditions</a>
-										</label>
+					</div>
+					<?php if (strcmp($usuario['nombre'], $this->session->userdata("usuario")) == 0): ?>
+						<div class="tab-pane" id="settings">
+							<form class="form-horizontal">
+								<div class="form-group">
+									<label for="txtNombre" class="col-sm-2 control-label">Nombre</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" id="txtNombre" value="<?php echo $usuario['nombre'] ?>">
 									</div>
 								</div>
-							</div>
-							<div class="form-group">
-								<div class="col-sm-offset-2 col-sm-10">
-									<button type="submit" class="btn btn-danger">Submit</button>
+								<div class="form-group">
+									<label for="txtAPaterno" class="col-sm-2 control-label">Apellido Paterno</label>
+									<div class="col-sm-10">
+										<input type="email" class="form-control" id="txtAPaterno" value="<?php echo $usuario['a_paterno'] ?>">
+									</div>
 								</div>
-							</div>
-						</form>
-					</div>		
+								<div class="form-group">
+									<label for="txtAMaterno" class="col-sm-2 control-label">Apellido Materno</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" id="txtAMaterno" value="<?php echo $usuario['a_materno'] ?>">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="txtCorreo" class="col-sm-2 control-label">Correo</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" id="txtCorreo" value="<?php echo $usuario['correo'] ?>">
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="txtUsuario" class="col-sm-2 control-label">Usuario</label>
+									<div class="col-sm-10">
+										<input type="text" class="form-control" id="txtUsuario" value="<?php echo $usuario['usuario'] ?>">
+									</div>
+								</div>
+								<div class="form-group row">
+									<label for="txtDescripcion" class="col-sm-2" style="padding-left: 0px; margin-right: 0px; padding-right: 0px; width: 10%; margin-left: 6.5%;">Descripción</label>
+									<div class="col-sm-10">
+										<textarea type="text" name="txtDescripcion" class="form-control" id="txtDescripcion">
+											<?php echo $usuario['descripcion'] ?>
+										</textarea>
+									</div>
+								</div>
+								<div class="form-group">
+									<button type="button" class="btn btn-default" id="btn-cambiar">Cambiar usuario y/o contraseña</button>
+								</div>
+							</form>
+						</div>		
+					<?php endif; ?>
 				</div>		
 			</div>		
 		</div>	
@@ -290,3 +162,8 @@
 	v>
 </section>
 <?php $this->load->view("Global/footer") ?>
+<script>
+	$(document).ready( function () {
+		$("#txtDescripcion").text($("#txtDescripcion").text().trim())
+	})
+</script>
