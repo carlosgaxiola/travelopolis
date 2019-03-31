da8b72a9ce3f150416dffedbb42b776e8c432fbc
@ThePunisher1997 <2016030955@upsin.edu.mx>
2019-03-30 18:16:03 -0700

    sin foto

--- application/views/Global/aside.php
+++ application/views/Global/aside.php
@@ -1,12 +1,8 @@
 <aside class="main-sidebar bg-white">
     <section class="sidebar">
         <div class="user-panel">
-            <div class="pull-left image">
-                <?php if ($this->session->userdata("admin_active")): ?>
-                    <img src="<?php echo base_url("assets/images/users/".$this->session->userdata("url_foto_perfil")."/profile_60x60.jpg") ?>" class="user-image img-circle" alt="User Image">
-                <?php else: ?>
-                    <img src="<?php echo base_url("assets/images/logo.png") ?>" class="img-circle" alt="User Image">
-                <?php endif; ?>
+            <div class="pull-left image">            
+                <img src="<?php echo base_url("assets/images/logo.png") ?>" class="img-circle" alt="User Image">                <?php endif; ?>
             </div>
             <div class="pull-left info">
                 <p><?php echo $this->session->userdata("usuario") ?></p>
