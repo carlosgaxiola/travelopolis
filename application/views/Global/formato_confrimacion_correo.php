f0f12dd2795748d4639a638f43d491e81d4c1803
@ThePunisher1997 <2016030955@upsin.edu.mx>
2019-03-30 20:50:12 -0700

    formato de correo

---
+++ application/views/Global/formato_confrimacion_correo.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registro</title>
+</head>
+<body>
+	Bienvenido <?php $completo ?>
+	Token de confirmación: <?php $token ?>
+</body>
+</html>
