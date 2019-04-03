b8c0f3e80a213e22bd0083b49a4733d1fd80437a
carlosgaxiola <2016030023@upsin.edu.mx>
2019-04-03 03:50:57 -0700

    Update tabla.php

--- application/views/administrar/detalle/tabla.php
+++ application/views/administrar/detalle/tabla.php
@@ -13,9 +13,9 @@
     	<?php if (isset($registros) and !empty($registros) and is_array($registros)): ?>            
     		<?php foreach ($registros as $index => $viajero): ?>
                 <?php
-                    $btnVerAcompañantes = "&nbsp;<button title='Ver datos' class='btn btn-primary btn-ver' type='button'><i class='fas fa-user'></i></button>";
+                    $btnVerAcompañantes = "<button title='Ver datos' class='btn btn-primary btn-ver' type='button'><i class='fas fa-user'></i></button>";
                     if ($viajero['cantidad'] > 1)
-                        $btnVerAcompañantes = "&nbsp;<button class='btn btn-primary btn-ver' type='button' title='Ver acompañantes'><i class='fas fa-users'></i></button>";                    
+                        $btnVerAcompañantes = "<button class='btn btn-primary btn-ver' type='button' title='Ver acompañantes'><i class='fas fa-users'></i></button>";                    
                     if ($viajero['status'] == 0) {
                         $label = "<span class='label label-danger'>Cancelado</span>";
                         $btnCancelar = "";
@@ -24,12 +24,12 @@
                     else if ($viajero['status'] == 1) {
                         $label = "<span class='label label-warning'>Cotización enviada</span>";
                         $btnCancelar = "<button title='Cancelar' class='btn btn-danger btn-cancelar' type='button'><i class='fas fa-ban'></i></button>";
-                        $btnAbonar = "&nbsp;<button type='button' class='btn btn-success btn-abonar'><i class='fas fa-money-bill-wave'></i></button>";
+                        $btnAbonar = "&nbsp;<button type='button' class='btn btn-success btn-abonar'><i class='fas fa-money-bill-wave'></i></button>&nbsp;";
                     }
                     else if ($viajero['status'] == 2) {
                         $label = "<span class='label label-primary'>Anticipo dado</span>";
                         $btnCancelar = "<button title='Cancelar' type='button' class='btn btn-danger btn-cancelar'><i class='fas fa-ban'></i></button>";
-                        $btnAbonar = "&nbsp;<button type='button' class='btn btn-success btn-abonar'><i class='fas fa-money-bill-wave'></i></button>";
+                        $btnAbonar = "&nbsp;<button type='button' class='btn btn-success btn-abonar'><i class='fas fa-money-bill-wave'></i></button>&nbsp;";
                     }
                     else if ($viajero['status'] == 3) {                        
                         $label = "<span class='label label-success'>Pagado</span>";
